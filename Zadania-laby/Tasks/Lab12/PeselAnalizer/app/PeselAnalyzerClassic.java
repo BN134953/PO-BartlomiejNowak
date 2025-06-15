fe932b3d15804a26fd850e657dd7edeabb26a947
KrwawyKastrator <taxiwrumwrum@gmail.com>
2025-06-15 21:41:25 +0200

    Delete Zadania-laby directory

--- Zadania-laby/Tasks/Lab12/PeselAnalizer/app/PeselAnalyzerClassic.java
+++
@@ -1,4 +0,0 @@
-package Tasks.Lab12.PeselAnalizer.app;
-
-public class PeselAnalyzerClassic {
-}
--- Zadania-laby/Tasks/Lab8/BlednaWartoscDlaSilniException.java
+++
@@ -1,7 +0,0 @@
-package Tasks.Lab8;
-
-public class BlednaWartoscDlaSilniException extends Exception {
-    public BlednaWartoscDlaSilniException(String message) {
-        super(message);
-    }
-}
